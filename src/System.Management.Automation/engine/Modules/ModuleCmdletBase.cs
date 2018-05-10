adityapatwardhan/PowerShell

commit ee27c5d121fc41338dba657cc4e38d6bdd96bdc4

Clean up workflow logic in the module loading component (#6523)
Clean up workflow logic in the module loading component.
Workflow module is not supported in PSCore.
Currently, Import-Module throws a terminating error when seeing .xaml modules. After the change, Import-Module throws a non-terminating error when seeing .xaml modules.

src/System.Management.Automation/engine/Modules/PSModuleInfo.cs
@@ -575,29 +575,19 @@ namespace System.Management.Automation
             }
 
             var res = new Dictionary<string, TypeDefinitionAst>(StringComparer.OrdinalIgnoreCase);
-            if (this.NestedModules != null)
+            foreach (var nestedModule in this.NestedModules)
             {
-                foreach (var nestedModule in this.NestedModules)
+                foreach (var typePairs in nestedModule.GetExportedTypeDefinitions())
                 {
-                    if (nestedModule == this)
-                    {
-                        // this is totally bizzare, but it happens for some reasons for
-                        // Microsoft.Powershell.Workflow.ServiceCore.dll, when there is a workflow defined in a nested module.
-                        // TODO(sevoroby): we should handle possible circular dependencies
-                        continue;
-                    }
-
-                    foreach (var typePairs in nestedModule.GetExportedTypeDefinitions())
-                    {
-                        // The last one name wins! It's the same for command names in nested modules.
-                        // For rootModule C with Two nested modules (A, B) the order is: A, B, C
-                        res[typePairs.Key] = typePairs.Value;
-                    }
-                }
-                foreach (var typePairs in _exportedTypeDefinitionsNoNested)
-                {
+                    // The last one name wins! It's the same for command names in nested modules.
+                    // For rootModule C with Two nested modules (A, B) the order is: A, B, C
                     res[typePairs.Key] = typePairs.Value;
                 }
+            }
+
+            foreach (var typePairs in _exportedTypeDefinitionsNoNested)
+            {
+                res[typePairs.Key] = typePairs.Value;
             }
 
             return new ReadOnlyDictionary<string, TypeDefinitionAst>(res);
@@ -640,8 +630,6 @@ namespace System.Management.Automation
         internal Collection<string> DeclaredFunctionExports = null;
         internal List<string> _detectedFunctionExports = new List<string>();
 
-        internal List<string> _detectedWorkflowExports = new List<string>();
-
         /// <summary>
         /// Add function to the fixed exports list
         /// </summary>
@@ -653,20 +641,6 @@ namespace System.Management.Automation
             if (!_detectedFunctionExports.Contains(name))
             {
                 _detectedFunctionExports.Add(name);
-            }
-        }
-
-        /// <summary>
-        /// Add workflow to the fixed exports list
-        /// </summary>
-        /// <param name="name">the function to add</param>
-        internal void AddDetectedWorkflowExport(string name)
-        {
-            Dbg.Assert(name != null, "AddDetectedWorkflowExport should not be called with a null value");
-
-            if (!_detectedWorkflowExports.Contains(name))
-            {
-                _detectedWorkflowExports.Add(name);
             }
         }
 
@@ -756,15 +730,6 @@ namespace System.Management.Automation
                     foreach (var function in functions)
                     {
                         exports[function.Key] = function.Value;
-                    }
-                }
-
-                Dictionary<string, FunctionInfo> workflows = this.ExportedWorkflows;
-                if (workflows != null)
-                {
-                    foreach (var workflow in workflows)
-                    {
-                        exports[workflow.Key] = workflow.Value;
                     }
                 }
 
@@ -1177,48 +1142,9 @@ namespace System.Management.Automation
         {
             get
             {
-                Dictionary<string, FunctionInfo> exportedWorkflows = new Dictionary<string, FunctionInfo>(StringComparer.OrdinalIgnoreCase);
-
-                if ((DeclaredWorkflowExports != null) && (DeclaredWorkflowExports.Count > 0))
-                {
-                    foreach (string fn in DeclaredWorkflowExports)
-                    {
-                        WorkflowInfo tempWf = new WorkflowInfo(fn, ScriptBlock.EmptyScriptBlock, context: null) { Module = this };
-                        exportedWorkflows[fn] = tempWf;
-                    }
-                }
-                if ((DeclaredWorkflowExports != null) && (DeclaredWorkflowExports.Count == 0))
-                {
-                    return exportedWorkflows;
-                }
-                else
-                {
-                    // If there is no session state object associated with this list,
-                    // just return a null list of exports. This will be true if the
-                    // module is a compiled module.
-                    if (SessionState == null)
-                    {
-                        foreach (string detectedExport in _detectedWorkflowExports)
-                        {
-                            if (!exportedWorkflows.ContainsKey(detectedExport))
-                            {
-                                WorkflowInfo tempWf = new WorkflowInfo(detectedExport, ScriptBlock.EmptyScriptBlock, context: null) { Module = this };
-                                exportedWorkflows[detectedExport] = tempWf;
-                            }
-                        }
-                        return exportedWorkflows;
-                    }
-
-                    foreach (WorkflowInfo wi in SessionState.Internal.ExportedWorkflows)
-                    {
-                        exportedWorkflows[wi.Name] = wi;
-                    }
-                }
-
-                return exportedWorkflows;
+                return new Dictionary<string, FunctionInfo>(StringComparer.OrdinalIgnoreCase);
             }
         }
-        internal Collection<string> DeclaredWorkflowExports = null;
 
         /// <summary>
         ///

test/powershell/Modules/Microsoft.PowerShell.Core/Import-Module.Tests.ps1
@@ -146,7 +146,7 @@ namespace ModuleCmdlets
 
         $psdFile = Join-Path $TESTDRIVE test.psd1
         $nestedModule = Join-Path NOExistedPath Microsoft.PowerShell.Commands.Utility.dll
-        New-ModuleManifest -Path $psdFile -NestedModules $nestedModule 
+        New-ModuleManifest -Path $psdFile -NestedModules $nestedModule
         try
         {
             $module = Import-Module $psdFile -PassThru
@@ -158,7 +158,7 @@ namespace ModuleCmdlets
         {
             Remove-Module $module -ErrorAction SilentlyContinue
         }
-        
+
     }
  }
 
@@ -198,5 +198,36 @@ Describe "Import-Module should be case insensitive" -Tags 'CI' {
         mytest | Should -BeExactly "hello"
         Remove-Module TestModule
         Get-Module tESTmODULE | Should -BeNullOrEmpty
+    }
+}
+
+Describe "Workflow .Xaml module is not supported in PSCore" -tags "Feature" {
+    BeforeAll {
+        $xamlFile = Join-Path $TestDrive "XamlTest.xaml"
+        New-Item -Path $xamlFile -ItemType File -Force
+
+        $xamlRootModule = Join-Path $TestDrive "XamlRootModule"
+        New-Item -Path $xamlRootModule -ItemType Directory -Force
+        Copy-Item $xamlFile $xamlRootModule
+        $xamlRootModuleManifest = Join-Path $xamlRootModule "XamlRootModule.psd1"
+        New-ModuleManifest -Path $xamlRootModuleManifest -RootModule "XamlTest.xaml"
+
+        $xamlNestedModule = Join-Path $TestDrive "XamlNestedModule"
+        New-Item -Path $xamlNestedModule -ItemType Directory -Force
+        Copy-Item $xamlFile $xamlNestedModule
+        $xamlNestedModuleManifest = Join-Path $xamlNestedModule "XamlNestedModule.psd1"
+        New-ModuleManifest -Path $xamlNestedModuleManifest -NestedModules "XamlTest.xaml"
+    }
+
+    It "Import a XAML file directly should raise a 'NotSupported' error" {
+        { Import-Module $xamlFile -ErrorAction Stop } | Should -Throw -ErrorId "Modules_WorkflowModuleNotSupported,Microsoft.PowerShell.Commands.ImportModuleCommand"
+    }
+
+    It "Import a module with XAML root module should raise a 'NotSupportd' error" {
+        { Import-Module $xamlRootModule -ErrorAction Stop } | Should -Throw -ErrorId "Modules_WorkflowModuleNotSupported,Microsoft.PowerShell.Commands.ImportModuleCommand"
+    }
+
+    It "Import a module with XAML nested module should raise a 'NotSupported' error" {
+        { Import-Module $xamlNestedModule -ErrorAction Stop } | Should -Throw -ErrorId "Modules_WorkflowModuleNotSupported,Microsoft.PowerShell.Commands.ImportModuleCommand"
     }
 }

src/System.Management.Automation/engine/ExecutionContext.cs
@@ -366,7 +366,7 @@ namespace System.Management.Automation
             };
 
         /// <summary>
-        /// Is true the PSScheduledJob and PSWorkflow modules are loaded for this runspace
+        /// Is true if the PSScheduledJob module is loaded for this runspace
         /// </summary>
         internal bool IsModuleWithJobSourceAdapterLoaded
         {

src/System.Management.Automation/engine/GetCommandCommand.cs
@@ -26,7 +26,7 @@ namespace Microsoft.PowerShell.Commands
     [Cmdlet(VerbsCommon.Get, "Command", DefaultParameterSetName = "CmdletSet", HelpUri = "https://go.microsoft.com/fwlink/?LinkID=113309")]
     [OutputType(typeof(AliasInfo), typeof(ApplicationInfo), typeof(FunctionInfo),
                 typeof(CmdletInfo), typeof(ExternalScriptInfo), typeof(FilterInfo),
-                typeof(WorkflowInfo), typeof(string), typeof(PSObject))]
+                typeof(string), typeof(PSObject))]
     public sealed class GetCommandCommand : PSCmdlet
     {
         #region Definitions of cmdlet parameters

src/System.Management.Automation/engine/Modules/AnalysisCache.cs
@@ -72,10 +72,6 @@ namespace System.Management.Automation
                 {
                     result = AnalyzeCdxmlModule(modulePath, context, lastWriteTime);
                 }
-                else if (extension.Equals(StringLiterals.WorkflowFileExtension, StringComparison.OrdinalIgnoreCase))
-                {
-                    result = AnalyzeXamlModule(modulePath, context, lastWriteTime);
-                }
                 else if (extension.Equals(".dll", StringComparison.OrdinalIgnoreCase))
                 {
                     result = AnalyzeDllModule(modulePath, context, lastWriteTime);
@@ -364,11 +360,6 @@ namespace System.Management.Automation
             s_cacheData.Entries[modulePath] = moduleCacheEntry;
 
             return result;
-        }
-
-        private static ConcurrentDictionary<string, CommandTypes> AnalyzeXamlModule(string modulePath, ExecutionContext context, DateTime lastWriteTime)
-        {
-            return AnalyzeTheOldWay(modulePath, context, lastWriteTime);
         }
 
         private static ConcurrentDictionary<string, CommandTypes> AnalyzeCdxmlModule(string modulePath, ExecutionContext context, DateTime lastWriteTime)

src/System.Management.Automation/engine/Modules/ImportModuleCommand.cs
@@ -531,11 +531,6 @@ namespace Microsoft.PowerShell.Commands
         {
             try
             {
-                if (name.Equals("PSWorkflow", StringComparison.OrdinalIgnoreCase) && Utils.IsRunningFromSysWOW64())
-                {
-                    throw new NotSupportedException(AutomationExceptions.WorkflowDoesNotSupportWOW64);
-                }
-
                 bool found = false;
                 PSModuleInfo foundModule = null;
 

src/System.Management.Automation/help/CommandHelpProvider.cs
@@ -124,9 +124,7 @@ namespace System.Management.Automation
                         }
                         else if (scriptCommandInfo != null &&
                                   (nestedModule.ExportedFunctions.ContainsKey(commandInfo.Name) ||
-                                    nestedModule.ExportedWorkflows.ContainsKey(commandInfo.Name) ||
-                                    (testWithoutPrefix && nestedModule.ExportedFunctions.ContainsKey(cmdNameWithoutPrefix)) ||
-                                    (testWithoutPrefix && nestedModule.ExportedWorkflows.ContainsKey(cmdNameWithoutPrefix))))
+                                    (testWithoutPrefix && nestedModule.ExportedFunctions.ContainsKey(cmdNameWithoutPrefix))))
                         {
                             nestedModulePath = nestedModule.Path;
                             break;

commit 5d073b8010bef75ddd06e8bcffd4ccdef086401e

Fix a module-loading regression that caused an infinite loop (#6843)
This regression was introduced by #6523, in `PSModuleInfo.cs`. A circular nested module check was removed because the comment there suggested it happens only with a deprecated workflow module. This causes a `StackOverflow` exception when running into circular nested modules.

Circular nested modules could happen for a module that is not well structured. For example, the module folder `test` contains two files: `test.psd1` and `test.psm1`, and `test.psd1` has the following content:
   @{ ModuleVersion = '0.0.1'; RootModule = 'test'; NestedModules = @('test') }

The same value `test` is put in both RootModule and NestedModule, which will end up with a module whose nested module points to itself.

There are two changes in this PR:
1. Add back the check for circular nested modules in `PSModuleInfo.cs`.
2. Remove a wrong `Dbg.Assert` in `ModuleCmdletBase.cs` and two checks before it.
   - For the assertion `Dbg.Assert(newManifestInfo.SessionState == ss`, when facing the example above, the nested module will first be loaded with a different session state, and then when trying to load the root module, the same loaded nested module will be reused for it. So 'newManifestInfo.SessionState' is not `ss`. The assertion will fail in that case.
   - For the two checks before the assertion, they are not needed anymore based on the comments there.

src/System.Management.Automation/engine/Modules/ModuleCmdletBase.cs
@@ -2820,7 +2820,7 @@ namespace Microsoft.PowerShell.Commands
                 {
                     bool found = false;
                     // Never load nested modules to the global scope.
-                    bool oldGLobal = this.BaseGlobal;
+                    bool oldGlobal = this.BaseGlobal;
                     this.BaseGlobal = false;
 
                     PSModuleInfo nestedModule = LoadModuleNamedInManifest(
@@ -2838,7 +2838,7 @@ namespace Microsoft.PowerShell.Commands
                             out found,
                             shortModuleName: null);
 
-                    this.BaseGlobal = oldGLobal;
+                    this.BaseGlobal = oldGlobal;
 
                     // If found, add it to the parent's list of NestedModules
                     if (found)
@@ -2976,30 +2976,16 @@ namespace Microsoft.PowerShell.Commands
                     BaseCmdletPatterns = oldCmdletPatterns;
                 }
 
-                // If there is an existing session state and the new module info
-                // session state is empty, then use the existing session state.
-                // This will be the case when ModuleToProcess is a binary module
-                // and there were nested modules.
-                if (newManifestInfo.SessionState == null && ss != null)
-                {
-                    newManifestInfo.SessionState = ss;
-                    ss.Internal.Module = newManifestInfo;
-                }
-                // If the new module info session state is not empty but there is no
-                // existing session state, then use the new module info's sessions state;
-                // This will be the case if the module to process is a script module
-                // but there where no nested modules.
-                else if (newManifestInfo.SessionState != null && ss == null)
-                {
-                    ss = newManifestInfo.SessionState;
-                }
-
-                // We don't need to care if they are both null which will be the
-                // case when there is only a binary module in ModuleToProcess and
-                // no nested modules. At this point the moduleInfo and the value in
-                // ss should be identical.
-                Dbg.Assert(newManifestInfo.SessionState == ss,
-                    "ss and newManifestInfo.SessionState should be the same after handling ModuleToProcess");
+                // For most cases, 'newManifestInfo.SessionState' should be identical to 'ss':
+                //  1. when 'importingModule == true', 'newManifestInfo' uses the same session state as 'ss' because we passed in 'ss' when loading the RootModule via 'LoadModuleNamedInManifest'.
+                //  2. when 'importingModule == false', both session states will be null since we are in module analysis mode (Get-Module -ListAvailable).
+                //
+                // However, there is one exception when the RootModule is also put in NestedModules in the module manifest (ill-organized module structure).
+                // For example, module folder 'test' contains two files: 'test.psd1' and 'test.psm1', and 'test.psd1' has the following content:
+                //     "@{ ModuleVersion = '0.0.1'; RootModule = 'test'; NestedModules = @('test') }"
+                //
+                // In that case, the nested module will first be loaded with a different session state, and then when trying to load the RootModule via 'LoadModuleNamedInManifest',
+                // the same loaded nested module will be reused for the RootModule by 'LoadModuleNamedInManifest'.
 
                 // Change the module name to match the manifest name, not the original name
                 newManifestInfo.SetName(manifestInfo.Name);
@@ -3248,7 +3234,6 @@ namespace Microsoft.PowerShell.Commands
                 {
                     if ((exportedCmdlets != null) && (ss != null))
                     {
-                        Dbg.Assert(ss.Internal.ExportedCmdlets != null, "ss.Internal.ExportedCmdlets should not be null");
                         manifestInfo.ExportedCmdlets.Clear();
 
                         // Mark stuff for export

src/System.Management.Automation/engine/Modules/PSModuleInfo.cs
@@ -577,6 +577,15 @@ namespace System.Management.Automation
             var res = new Dictionary<string, TypeDefinitionAst>(StringComparer.OrdinalIgnoreCase);
             foreach (var nestedModule in this.NestedModules)
             {
+                if (nestedModule == this)
+                {
+                    // Circular nested modules could happen with ill-organized module structure.
+                    // For example, module folder 'test' has two files: 'test.psd1' and 'test.psm1', and 'test.psd1' has the following content:
+                    //    "@{ ModuleVersion = '0.0.1'; RootModule = 'test'; NestedModules = @('test') }"
+                    // Then, 'Import-Module test.psd1 -PassThru' will return a ModuleInfo object with circular nested modules.
+                    continue;
+                }
+
                 foreach (var typePairs in nestedModule.GetExportedTypeDefinitions())
                 {
                     // The last one name wins! It's the same for command names in nested modules.

test/powershell/Modules/Microsoft.PowerShell.Core/Import-Module.Tests.ps1
@@ -241,3 +241,25 @@ Describe "Workflow .Xaml module is not supported in PSCore" -tags "Feature" {
         { Import-Module $xamlNestedModule -ErrorAction Stop } | Should -Throw -ErrorId "Modules_WorkflowModuleNotSupported,Microsoft.PowerShell.Commands.ImportModuleCommand"
     }
 }
+
+Describe "Circular nested module test" -tags "CI" {
+    BeforeAll {
+        $moduleFolder = Join-Path $TestDrive CircularNestedModuleTest
+        $psdPath = Join-Path $moduleFolder CircularNestedModuleTest.psd1
+        $psmPath = Join-Path $moduleFolder CircularNestedModuleTest.psm1
+
+        New-Item -Path $moduleFolder -ItemType Directory -Force > $null
+        Set-Content -Path $psdPath -Value "@{ ModuleVersion = '0.0.1'; RootModule = 'CircularNestedModuleTest'; NestedModules = @('CircularNestedModuleTest') }" -Encoding Ascii
+        Set-Content -Path $psmPath -Value "function bar {}" -Encoding Ascii
+    }
+
+    AfterAll {
+        Remove-Module -Name CircularNestedModuleTest -Force -ErrorAction SilentlyContinue
+        Remove-Item -Path $moduleFolder -Force -Recurse
+    }
+
+    It "Loading the module should succeed and return a module with circular nested module" {
+        $m = Import-Module $psdPath -PassThru
+        $m.NestedModules[0] | Should -Be $m
+    }
+}