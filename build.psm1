062b8445aa4f1b8f7750da1864606e0389c074a7
Aditya Patwardhan <adityap@microsoft.com>
2018-07-26 12:04:43 -0700

    Update build.psm1

--- build.psm1
+++ build.psm1
@@ -5,6 +5,8 @@
 # On Windows paths is separated by semicolon
 $script:TestModulePathSeparator = [System.IO.Path]::PathSeparator
 
+Write-Host "Hello"
+
 $dotnetCLIChannel = "release"
 $dotnetCLIRequiredVersion = $(Get-Content $PSScriptRoot/global.json | ConvertFrom-Json).Sdk.Version
 
