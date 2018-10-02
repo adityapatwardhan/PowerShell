b8258eb27059840943ae2d86f19860c0c0909f0e
Aditya Patwardhan <adityap@microsoft.com>
2018-10-01 17:05:47 -0700

    Markdown content parsing

--- src/System.Management.Automation/help/MarkdownHelpNode.cs
+++ src/System.Management.Automation/help/MarkdownHelpNode.cs
@@ -2,6 +2,7 @@
 // Licensed under the MIT License.
 
 using System;
+using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using Microsoft.PowerShell.MarkdownRender;
 
@@ -46,14 +47,46 @@ namespace System.Management.Automation
                 MarkdownConversionType.VT100,
                 mdOption);
 
-            dynamic headerToken = mdInfo.Tokens[0];
-            string cmdletName = headerToken?.Inline?.FirstChild?.ToString();
+            var mdBlocksDictionary = GetParsedMarkdownContentBlocks(mdInfo);
 
             var detailsObj = new PSObject();
-            detailsObj.AddOrSetProperty("name", new PSObject(cmdletName));
+
+            if(mdBlocksDictionary.TryGetValue("Name", out string cmdletName))
+            {
+                detailsObj.AddOrSetProperty("name", new PSObject(cmdletName));
+            }
+
             retObj.AddOrSetProperty("Details", detailsObj);
 
             return retObj;
+        }
+
+        private Dictionary<string, string> GetParsedMarkdownContentBlocks(MarkdownInfo mdInfo)
+        {
+            Dictionary<string, string> markdownDictionary = new Dictionary<string, string>(StringComparer.OrdinalIgnoreCase);
+
+            if (mdInfo?.Tokens.Count > 0)
+            {
+                dynamic headerToken = mdInfo?.Tokens[0];
+                string cmdletName = headerToken?.Inline?.FirstChild?.ToString();
+                markdownDictionary.Add("Name", cmdletName);
+                markdownDictionary.Add("FormattedName", GetFormattedCmdletName(cmdletName, mdInfo.VT100EncodedString));
+            }
+
+            return markdownDictionary;
+        }
+
+        private string GetFormattedCmdletName(string cmdletName, string vt100EncodedString)
+        {
+            var lines = vt100EncodedString.Split("\n");
+            if (lines.Length > 0)
+            {
+                return lines[0];
+            }
+            else
+            {
+                throw new ArgumentNullException("vt100EncodedString is null or empty");
+            }
         }
     }
 }
